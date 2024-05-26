25f16fc0b65afcba555a53beb70d8c22ad3d6ae7
Uxie_Gui <111542995+UxieGu1@users.noreply.github.com>
2024-05-26 10:31:25 -0300

    Update registrarUser.php

--- Login e Validação/registrarUser.php
+++ Login e Validação/registrarUser.php
@@ -1,32 +1,25 @@
 <?php
     $servername = "localhost";
-    $username = "root"; // Substitua pelo seu nome de usuário do MySQL
-    $password = ""; // Substitua pela sua senha do MySQL
-    $database = "projeto_lbs"; // Substitua pelo nome do seu banco de dados
-
-    // Criar conexão com o banco de dados
+    $username = "root"; 
+    $password = ""; 
+    $database = "projeto_lbs"; 
     $conn = new mysqli($servername, $username, $password, $database);
 
-    // Verificar se a conexão foi estabelecida corretamente
     if ($conn->connect_error) {
         die("Ocorreu um erro: " . $conn->connect_error);
     }
 
-    // Capturar os valores do formulário de registro
     $login = $_POST['login'];
     $senha = $_POST['senha'];
     $nome_completo = $_POST['nome'];
     $data_nascimento = $_POST['data'];
     $email = $_POST['email'];
 
-    // Verificar se todos os campos foram preenchidos
     if (empty($login) || empty($senha) || empty($nome_completo) || empty($data_nascimento) || empty($email)) {
         echo "Por favor, preencha todos os campos.";
     } else {
-        // Consulta SQL para inserir os dados do novo usuário no banco de dados
         $insert_sql = "INSERT INTO usuario (Login, Senha, NomeCompleto, DataNascimento, Email) VALUES ('$login', '$senha', '$nome_completo', '$data_nascimento', '$email')";
 
-        // Executar a consulta SQL
         if ($conn->query($insert_sql) === TRUE) {
             echo "Novo usuário adicionado com sucesso.";
         } else {
@@ -34,6 +27,5 @@
         }
     }
 
-    // Fechar conexão com o banco de dados
     $conn->close();
 ?>
