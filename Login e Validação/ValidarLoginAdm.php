0b3a4bb894ec2cfbd360bda3e0624f5e3d801cd8
Uxie_Gui <111542995+UxieGu1@users.noreply.github.com>
2024-05-26 10:31:56 -0300

    Update ValidarLoginAdm.php

--- Login e Validação/ValidarLoginAdm.php
+++ Login e Validação/ValidarLoginAdm.php
@@ -1,41 +1,30 @@
 <?php
     $servername = "localhost";
-    $username = "root"; // Substitua pelo seu nome de usuário do MySQL
-    $password = ""; // Substitua pela sua senha do MySQL
-    $database = "projeto_lbs"; // Substitua pelo nome do seu banco de dados
-
-    // Criar conexão com o banco de dados
+    $username = "root"; 
+    $password = ""; 
+    $database = "projeto_lbs"; 
     $conn = new mysqli($servername, $username, $password, $database);
 
-    // Verificar se a conexão foi estabelecida corretamente
     if ($conn->connect_error) {
         die("Ocorreu um erro: " . $conn->connect_error);
     }
 
-    // Capturar os valores do formulário
     $login = $_POST['login'];
     $senha = $_POST['senha'];
 
-    // Verificar se os campos estão preenchidos
     if (empty($login) || empty($senha)) {
         echo "Por favor, preencha todos os campos.";
     } else {
-        // Consulta SQL para validar o login do administrador
         $sql = "SELECT * FROM administrador WHERE Login='$login' AND Senha='$senha'";
 
-        // Executar a consulta SQL
         $result = $conn->query($sql);
 
-        // Verificar se a consulta retornou algum resultado
         if ($result->num_rows > 0) {
-            // Login bem-sucedido
             echo "Login de administrador bem-sucedido.";
         } else {
-            // Login inválido
             echo "Login de administrador inválido.";
         }
     }
 
-    // Fechar conexão com o banco de dados
     $conn->close();
 ?>
