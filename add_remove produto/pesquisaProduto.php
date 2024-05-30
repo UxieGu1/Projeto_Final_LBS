fb08e7f327abc9bd5170da20e59b25ae66489ed8
Guilherme <111542995+UxieGu1@users.noreply.github.com>
2024-05-30 11:01:25 -0300

    Update pesquisaProduto.php

--- add_remove produto/pesquisaProduto.php
+++ add_remove produto/pesquisaProduto.php
@@ -1,37 +1,36 @@
 <?php
 if(isset($_POST['produto']) && isset($_POST['descricao']) && isset($_POST['preco'])) {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "projeto_lbs";
+    $servidor = "localhost";
+    $usuario = "root"; 
+    $senhaBDD = ""; 
+    $bancoDD = "projeto_lbs";
+    $conexao = new mysqli($servidor, $usuario, $senhaBDD, $bancoDD);
 
-    $conn = new mysqli($servername, $username, $password, $database);
-
-    if ($conn->connect_error) {
-        die("Ocorreu um erro ao conectar ao banco de dados: " . $conn->connect_error);
+    if ($conexao->connect_error) {
+        die("Ocorreu um erro ao conectar ao banco de dados: " . $conexao->connect_error);
     }
 
-    $produto = $conn->real_escape_string($_POST['produto']);
-    $descricao = $conn->real_escape_string($_POST['descricao']);
-    $preco = $conn->real_escape_string($_POST['preco']);
+    $produto = $conexao->real_escape_string($_POST['produto']);
+    $descricao = $conexao->real_escape_string($_POST['descricao']);
+    $preco = $conexao->real_escape_string($_POST['preco']);
 
     $sql = "SELECT * FROM produto WHERE Nome LIKE '%$produto%' AND Descrição LIKE '%$descricao%' AND Preço <= $preco";
 
-    $result = $conn->query($sql);
+    $resultado = $conexao->query($sql);
 
-    if ($result->num_rows > 0) {
+    if ($resultado->num_rows > 0) {
         echo "<h2>Produtos Encontrados:</h2>";
-        while($row = $result->fetch_assoc()) {
-            echo "<p>Nome: " . $row["Nome"] . "</p>";
-            echo "<p>Descrição: " . $row["Descrição"] . "</p>";
-            echo "<p>Preço: " . $row["Preço"] . "</p>";
+        while($linha = $resultado->fetch_assoc()) {
+            echo "<p>Nome: " . $linha["Nome"] . "</p>";
+            echo "<p>Descrição: " . $linha["Descrição"] . "</p>";
+            echo "<p>Preço: " . $linha["Preço"] . "</p>";
             echo "<hr>";
         }
     } else {
         echo "Nenhum produto encontrado com os critérios fornecidos.";
     }
 
-    $conn->close();
+    $conexao->close();
 
 } else {
     echo "Por favor, preencha todos os campos do formulário.";
