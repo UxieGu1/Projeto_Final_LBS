cb3f82672ee9baebbae1f4f0176b107464072740
Guilherme <111542995+UxieGu1@users.noreply.github.com>
2024-06-04 18:50:24 -0300

    Update Projeto_LBS(FINAL).sql

--- Banco de dados/Projeto_LBS(FINAL).sql
+++ Banco de dados/Projeto_LBS(FINAL).sql
@@ -1,81 +1,37 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 26/05/2024 às 19:15
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+CREATE TABLE `Administrador` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `Nome` varchar(255),
+  `Senha` varchar(255)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE `Usuario` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `Nome` varchar(255),
+  `data_nascimento` varchar(255),
+  `email` varchar(255),
+  `Senha` varchar(255),
+  `Plano_id` int,
+  FOREIGN KEY (`Plano_id`) REFERENCES `PlanoExercicio` (`id`)
+);
 
+CREATE TABLE `Produto` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `Nome` varchar(255),
+  `Descricao` text,
+  `Preco` decimal
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE `PlanoExercicio` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `Nome` varchar(255),
+  `Descricao` text,
+  `Preco_mensal` decimal
+);
 
---
--- Banco de dados: `projeto_lbs`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `usuario`
---
-
-CREATE TABLE `usuario` (
-  `ID` int(11) NOT NULL,
-  `Login` varchar(255) DEFAULT NULL,
-  `Senha` varchar(255) DEFAULT NULL,
-  `Plano_ID` int(11) DEFAULT NULL,
-  `NomeCompleto` varchar(100) NOT NULL,
-  `DataNascimento` date DEFAULT NULL,
-  `Email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `usuario`
---
-
-INSERT INTO `usuario` (`ID`, `Login`, `Senha`, `Plano_ID`, `NomeCompleto`, `DataNascimento`, `Email`) VALUES
-(2, 'gui', '12345', NULL, 'Guilherme', NULL, 'sousaguiro@gmail.com');
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `plano_id` (`Plano_ID`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `usuario`
---
-ALTER TABLE `usuario`
-  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`Plano_ID`) REFERENCES `planoexercicio` (`ID`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE `Compra` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `Usuario_id` int,
+  `Produto_id` int,
+  FOREIGN KEY (`Usuario_id`) REFERENCES `Usuario` (`id`),
+  FOREIGN KEY (`Produto_id`) REFERENCES `Produto` (`id`)
+);
