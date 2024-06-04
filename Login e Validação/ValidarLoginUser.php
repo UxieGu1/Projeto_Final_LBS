bddb7640a2d2ab34819a98aba2fafbe61781112d
Guilherme <111542995+UxieGu1@users.noreply.github.com>
2024-06-04 18:46:52 -0300

    Update ValidarLoginUser.php

--- Login e Validação/ValidarLoginUser.php
+++ Login e Validação/ValidarLoginUser.php
@@ -1,38 +1,31 @@
 <?php
-    $servidor = "localhost";
-    $usuario = "root"; 
-    $senhaBDD = ""; 
-    $bancoDD = "projeto_lbs"; 
-    $conexao = new mysqli($servidor, $usuario, $senhaBDD, $bancoDD);
+$servidor = "localhost";
+$usuario = "root"; 
+$senhaBDD = ""; 
+$bancoDD = "projeto_lbs"; 
+$conexao = new mysqli($servidor, $usuario, $senhaBDD, $bancoDD);
 
-    if ($conexao->connect_error) {
-        die("Ocorreu um erro: " . $conexao->connect_error);
-    }
+if ($conexao->connect_error) {
+    die("Ocorreu um erro: " . $conexao->connect_error);
+}
 
-    $login = $_POST['login'];
-    $senha = $_POST['senha'];
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
 
-    if (empty($login) || empty($senha)) {
-        echo "Por favor, preencha todos os campos.";
-        exit(); 
-    }
+if (empty($login) || empty($email) || empty($senha)) {
+    echo "Por favor, preencha todos os campos.";
+    exit(); 
+}
 
-    $ver_sql = "SELECT * FROM usuario WHERE Login='$login'";
-    $ver_resultado = $conexao->query($ver_sql);
+$ver_sql = "SELECT * FROM usuario WHERE Nome='$nome' AND Email='$email' AND Senha='$senha'";
+$ver_resultado = $conexao->query($ver_sql);
 
-    if ($ver_resultado->num_rows > 0) {
-        $sql = "SELECT * FROM usuario WHERE Login='$login' AND Senha='$senha'";
-        $resultado = $conexao->query($sql);
+if ($ver_resultado->num_rows > 0) {
+    echo "Login de usuário bem-sucedido.";
+} else {
+    echo "Login, e-mail ou senha incorretos.";
+}
 
-        if ($resultado->num_rows > 0) {
-            echo "Login de usuário bem-sucedido.";
-        } else {
-            echo "Senha incorreta.";
-        }
-    } else {
-        header("Location: cadastroUser.html");
-        exit();
-    }
-
-    $conexao->close();
+$conexao->close();
 ?>
